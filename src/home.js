e168f8f5651bc35cab7255f5ce24925fe3cf6d13
Ali George <george7ali@gmail.com>
2023-12-02 01:01:32 +0100

    append fecthed data to createAppend to display

--- src/home.js
+++ src/home.js
@@ -1,8 +1,15 @@
+import createAppend from './htmlAndAppendData.js';
+
+const content = document.querySelector('#content');
 async function displayHomeResults() {
   const url = 'https://api.tvmaze.com/shows';
   try {
     const response = await fetch(url);
     const loadedTvShows = await response.json();
+    const randomNumber = Math.floor(Math.random() * 200);
+    (loadedTvShows.slice(randomNumber, randomNumber + 20)).forEach((show) => {
+      createAppend(content, show);
+    });
     return loadedTvShows;
   } catch (error) {
     return error;
