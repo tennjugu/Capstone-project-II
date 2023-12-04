c9a40b23c5016c5d66c2982ade6fe4a1df485e10
Ali George <george7ali@gmail.com>
2023-12-04 15:20:13 +0100

    create function to fetch data from api

---
+++ src/tvShows.js
@@ -0,0 +1,19 @@
+import createAppend from './moviesCard.js';
+
+const content = document.querySelector('#content');
+async function getShowsList() {
+  const url = 'https://api.tvmaze.com/shows';
+  try {
+    console.log('Fetching TV shows...');
+    const response = await fetch(url);
+    const loadedTvShows = await response.json();
+    loadedTvShows.forEach((show) => {
+      createAppend(content, show);
+    });
+    console.log(loadedTvShows);
+    return loadedTvShows;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
