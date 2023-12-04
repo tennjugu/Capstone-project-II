fdbd2e2d78620305bbe0d5a5887eba51e252efd5
Ali George <george7ali@gmail.com>
2023-12-04 15:28:26 +0100

    implement linters check correction

--- src/tvShows.js
+++ src/tvShows.js
@@ -4,16 +4,13 @@ const content = document.querySelector('#content');
 async function getShowsList() {
   const url = 'https://api.tvmaze.com/shows';
   try {
-    console.log('Fetching TV shows...');
     const response = await fetch(url);
     const loadedTvShows = await response.json();
     loadedTvShows.forEach((show) => {
       createAppend(content, show);
     });
-    console.log(loadedTvShows);
     return loadedTvShows;
   } catch (error) {
-    console.log(error);
     return error;
   }
 }
